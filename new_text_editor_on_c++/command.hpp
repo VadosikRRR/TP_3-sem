f2ef7eabce0b25e10b4ce0c62de3b0f83a8c5f10
VadosikRRR <rechkin.77@yandex.ru>
2025-02-07 21:13:11 +0300

    Added class InsertCommand

--- new_text_editor_on_c++/command.hpp
+++ new_text_editor_on_c++/command.hpp
@@ -12,11 +12,10 @@ public:
 
 class AddCommand : public Command {
 private:
-    int _index;
     std::string _new_text;
     std::string &_text;
 public:
-    AddCommand(std::string &text, std::string new_text, int index);
+    AddCommand(std::string &text, std::string new_text);
     virtual void Execute();
 };
 
@@ -28,5 +27,17 @@ private:
     std::string &_text;
 public:
     MoveCommand(std::string &text, int old_index, int new_text);
+    virtual void Execute();
+};
+
+
+class InsertCommand : public Command {
+private:
+    private:
+    int _index;
+    std::string _new_text;
+    std::string &_text;
+public:
+    InsertCommand(std::string &text, std::string new_text, int index);
     virtual void Execute();
 };
